1c3c8d3a6ee3257bd760b28e64592983eb651a3d
MarkoM-dot <83985775+MarkoM-dot@users.noreply.github.com>
2022-05-19 21:50:15 +0200

    add(env): remove envs from code

--- src/database.py
+++ src/database.py
@@ -3,10 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlmodel import SQLModel
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
+from src.config import get_settings
+
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    get_settings().db_url, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+
+from src.config import get_settings
 from .routers import questions, tools
 from .database import create_db_and_tables
 
-app = FastAPI(debug=True)
+app = FastAPI(debug=get_settings().debug)
 
 app.include_router(questions.router, prefix="/questions", tags=["Questions"])
 app.include_router(tools.router, prefix="/tools", tags=["Tools"])
